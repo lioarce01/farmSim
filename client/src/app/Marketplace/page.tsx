8276855fb12afd577e419da38403817cb8021faa
lioarce01 <lioarce1@gmail.com>
2024-10-17 19:14:46 -0300

    navbar added to marketplace component

--- client/src/app/Marketplace/page.tsx
+++ client/src/app/Marketplace/page.tsx
@@ -29,6 +29,7 @@ import Image from 'next/image';
 import LoadingSkeleton from './Skeleton';
 import { Pagination } from './Pagination';
 import { Rarity } from 'src/types';
+import Navbar from 'src/components/Navbar';
 
 const rarityColors = {
   [Rarity.LEGENDARY]: 'bg-yellow-500',
@@ -109,126 +110,131 @@ export default function Marketplace() {
   }
 
   return (
-    <div className="container mx-auto p-4 bg-background text-foreground text-white">
-      <header className="mb-8 flex justify-between items-center">
-        <h1 className="text-3xl font-bold">Marketplace</h1>
-      </header>
-      <div className="flex flex-col md:flex-row gap-8">
-        <aside className="md:block w-64 transition-all duration-300 ease-in-out">
-          <div className="flex gap-4 flex-col w-full sm:w-auto">
-            <Select value={selectedRarity} onValueChange={setSelectedRarity}>
-              <SelectTrigger className="w-full sm:w-[180px] bg-[#1a1a25] text-white">
-                <SelectValue placeholder="Select Rarity" />
-              </SelectTrigger>
-              <SelectContent className="bg-[#1a1a25] text-white">
-                <SelectItem value="all">All Rarities</SelectItem>
-                {Object.entries(rarityColors).map(([rarity, color]) => (
-                  <SelectItem key={rarity} value={rarity}>
-                    <span style={{ color }}>
-                      {rarity.charAt(0).toUpperCase() + rarity.slice(1)}
-                    </span>
-                  </SelectItem>
-                ))}
-              </SelectContent>
-            </Select>
-            <Select value={sortBy} onValueChange={setSortBy}>
-              <SelectTrigger className="w-full sm:w-[180px] bg-[#1a1a25] text-white">
-                <SelectValue placeholder="Sort by" />
-              </SelectTrigger>
-              <SelectContent className="bg-[#1a1a25] text-white text-center">
-                <SelectItem value="name">Name</SelectItem>
-                <SelectItem value="lowToHigh">Low to High</SelectItem>
-                <SelectItem value="highToLow">High to Low</SelectItem>
-              </SelectContent>
-            </Select>
-          </div>
-        </aside>
-        <main className="flex-1">
-          <div className="flex flex-col sm:flex-row justify-between items-center mb-4 gap-4">
-            <p className="text-sm text-muted-foreground">
-              Showing {sortedListings?.length || 0} results
-            </p>
-          </div>
-          <ScrollArea className="h-[calc(100vh-300px)] rounded-md border border-[#1f1f2c]">
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 p-6">
-              {paginatedListings ? (
-                paginatedListings?.map((item) => (
-                  <Card
-                    key={item.id}
-                    className="transition-all duration-300 ease-in-out hover:shadow-lg hover:scale-105 bg-[#1a1a25]"
-                  >
-                    <CardHeader>
-                      <CardTitle className="text-lg">{item.seedName}</CardTitle>
-                    </CardHeader>
-                    <CardContent>
-                      <div className="aspect-square bg-muted rounded-md mb-4 relative flex items-center justify-center border-2 border-[#2a2a3b] overflow-hidden">
-                        <Image
-                          src={bgPlant}
-                          alt="bgPlant"
-                          sizes="50vw"
-                          fill
-                          priority
-                          className="absolute inset-0 rounded-md blur-sm opacity-70 object-cover"
-                        />
-                        <Image
-                          src={item.seedImg || ''}
-                          alt={item.seedName || ''}
-                          width={200}
-                          height={200}
-                          className="relative z-10 object-contain w-3/4 h-3/4 "
-                        />
-                      </div>
-                      <div className="flex justify-between items-center">
-                        <p className="text-2xl font-bold">
-                          ${item.price.toFixed(2)}
-                        </p>
-                        <Badge
-                          className={`${rarityColors[item.seedRarity as keyof typeof rarityColors] || 'bg-gray-600'}`}
-                        >
-                          {item.seedRarity || 'Unknown'}
-                        </Badge>
-                      </div>
-                    </CardContent>
-                    <CardFooter>
-                      <Button
-                        className="w-full bg-[#222231] hover:bg-[#29293b] text-white transition duration-300"
-                        onClick={() => handleOpenPopup(item.id.toString())}
-                      >
-                        <Info className="mr-2 h-4 w-4" />
-                        Details
-                      </Button>
-                    </CardFooter>
-                  </Card>
-                ))
-              ) : (
-                <div className="text-center text-xl">
-                  <p>No listings found. Be the first to create one!</p>
-                  <Button
-                    className="w-full bg-[#1a1a25] text-white hover:bg-[#262630] transition duration-300"
-                    onClick={() => router.push('/CreateListing')}
-                  >
-                    Create Listing
-                  </Button>
-                </div>
-              )}
+    <>
+      <Navbar />
+      <div className="container mx-auto p-4 bg-background text-foreground text-white pt-20">
+        <header className="mb-8 flex justify-between items-center">
+          <h1 className="text-3xl font-bold">Marketplace</h1>
+        </header>
+        <div className="flex flex-col md:flex-row gap-8">
+          <aside className="md:block w-64 transition-all duration-300 ease-in-out">
+            <div className="flex gap-4 flex-col w-full sm:w-auto">
+              <Select value={selectedRarity} onValueChange={setSelectedRarity}>
+                <SelectTrigger className="w-full sm:w-[180px] bg-[#1a1a25] text-white">
+                  <SelectValue placeholder="Select Rarity" />
+                </SelectTrigger>
+                <SelectContent className="bg-[#1a1a25] text-white">
+                  <SelectItem value="all">All Rarities</SelectItem>
+                  {Object.entries(rarityColors).map(([rarity, color]) => (
+                    <SelectItem key={rarity} value={rarity}>
+                      <span style={{ color }}>
+                        {rarity.charAt(0).toUpperCase() + rarity.slice(1)}
+                      </span>
+                    </SelectItem>
+                  ))}
+                </SelectContent>
+              </Select>
+              <Select value={sortBy} onValueChange={setSortBy}>
+                <SelectTrigger className="w-full sm:w-[180px] bg-[#1a1a25] text-white">
+                  <SelectValue placeholder="Sort by" />
+                </SelectTrigger>
+                <SelectContent className="bg-[#1a1a25] text-white text-center">
+                  <SelectItem value="name">Name</SelectItem>
+                  <SelectItem value="lowToHigh">Low to High</SelectItem>
+                  <SelectItem value="highToLow">High to Low</SelectItem>
+                </SelectContent>
+              </Select>
             </div>
-          </ScrollArea>
-          <div className="mt-8">
-            <Pagination
-              currentPage={currentPage}
-              totalPages={totalPages}
-              onPageChange={setCurrentPage}
-            />
-          </div>
-          {listingId && (
-            <MarketListingPage
-              listingId={listingId}
-              onClose={handleClosePopup}
-              refetchMarketListings={refetchMarketListings}
-            />
-          )}
-        </main>
+          </aside>
+          <main className="flex-1">
+            <div className="flex flex-col sm:flex-row justify-between items-center mb-4 gap-4">
+              <p className="text-sm text-muted-foreground">
+                Showing {sortedListings?.length || 0} results
+              </p>
+            </div>
+            <ScrollArea className="h-[calc(100vh-300px)] rounded-md border border-[#1f1f2c]">
+              <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 p-6">
+                {paginatedListings ? (
+                  paginatedListings?.map((item) => (
+                    <Card
+                      key={item.id}
+                      className="transition-all duration-300 ease-in-out hover:shadow-lg hover:scale-105 bg-[#1a1a25]"
+                    >
+                      <CardHeader>
+                        <CardTitle className="text-lg">
+                          {item.seedName}
+                        </CardTitle>
+                      </CardHeader>
+                      <CardContent>
+                        <div className="aspect-square bg-muted rounded-md mb-4 relative flex items-center justify-center border-2 border-[#2a2a3b] overflow-hidden">
+                          <Image
+                            src={bgPlant}
+                            alt="bgPlant"
+                            sizes="50vw"
+                            fill
+                            priority
+                            className="absolute inset-0 rounded-md blur-sm opacity-70 object-cover"
+                          />
+                          <Image
+                            src={item.seedImg || ''}
+                            alt={item.seedName || ''}
+                            width={200}
+                            height={200}
+                            className="relative z-10 object-contain w-3/4 h-3/4 "
+                          />
+                        </div>
+                        <div className="flex justify-between items-center">
+                          <p className="text-2xl font-bold">
+                            ${item.price.toFixed(2)}
+                          </p>
+                          <Badge
+                            className={`${rarityColors[item.seedRarity as keyof typeof rarityColors] || 'bg-gray-600'}`}
+                          >
+                            {item.seedRarity || 'Unknown'}
+                          </Badge>
+                        </div>
+                      </CardContent>
+                      <CardFooter>
+                        <Button
+                          className="w-full bg-[#222231] hover:bg-[#29293b] text-white transition duration-300"
+                          onClick={() => handleOpenPopup(item.id.toString())}
+                        >
+                          <Info className="mr-2 h-4 w-4" />
+                          Details
+                        </Button>
+                      </CardFooter>
+                    </Card>
+                  ))
+                ) : (
+                  <div className="text-center text-xl">
+                    <p>No listings found. Be the first to create one!</p>
+                    <Button
+                      className="w-full bg-[#1a1a25] text-white hover:bg-[#262630] transition duration-300"
+                      onClick={() => router.push('/CreateListing')}
+                    >
+                      Create Listing
+                    </Button>
+                  </div>
+                )}
+              </div>
+            </ScrollArea>
+            <div className="mt-8">
+              <Pagination
+                currentPage={currentPage}
+                totalPages={totalPages}
+                onPageChange={setCurrentPage}
+              />
+            </div>
+            {listingId && (
+              <MarketListingPage
+                listingId={listingId}
+                onClose={handleClosePopup}
+                refetchMarketListings={refetchMarketListings}
+              />
+            )}
+          </main>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
